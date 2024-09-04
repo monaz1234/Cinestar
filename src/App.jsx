429bda355bd1fc3a54f524b990c4800d85ed224d
DuongTienSinh <huynhnhutduong7112003@gmail.com>
2024-09-04 23:04:31 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="w-24 h-24 bg-red-500">Hello</div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+};
