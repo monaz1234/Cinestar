6e25c2f82160ff69df0eb4a15b4931922bbeb190
monaz1234 <Chomon0011@gmail.com>
2024-09-20 16:18:23 +0700

    Update lại thông tin
    Update lại thông tin

--- .history/src/routes/AppRouter_20240920161423.jsx
+++ .history/src/routes/AppRouter_20240920161423.jsx
@@ -1,13 +1,13 @@
 // src/routers/AppRouter.jsx
 import { Routes, Route } from 'react-router-dom';
-import HomePage from '../pages/Homepage.jsx'; // Trang chủ
+import HomePage from '../pages/Homepage'; // Trang chủ
 import AboutPage from '../pages/AboutPage/index'; // Trang giới thiệu
 
 
 export default function AppRouter() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage/>} /> 
+      <Route path=" " element={<HomePage />} /> 
       <Route path="/about" element={<AboutPage />} />
 
     </Routes>
--- .history/src/routes/AppRouter_20240920161531.jsx
+++ .history/src/routes/AppRouter_20240920161531.jsx
@@ -7,8 +7,8 @@ import AboutPage from '../pages/AboutPage/index'; // Trang giới thiệu
 export default function AppRouter() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage/>} /> 
-      <Route path="/about" element={<AboutPage />} />
+      <Route path="/about" element={<HomePage />} /> 
+     
 
     </Routes>
   );
