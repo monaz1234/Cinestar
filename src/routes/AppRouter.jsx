b0ce13ed195496b9eaad4cd4bdab031c8de4ac66
monaz1234 <Chomon0011@gmail.com>
2024-09-20 16:11:38 +0700

    check home
    check home

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -8,7 +8,7 @@ import AboutPage from '../pages/AboutPage/index'; // Trang giới thiệu
 export default function AppRouter() {
   return (
     <Routes>
-      <Route path="/home" element={<HomePage />} /> 
+      <Route path="" element={<HomePage />} /> 
       <Route path="/about" element={<AboutPage />} />
 
     </Routes>
